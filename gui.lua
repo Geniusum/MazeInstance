6a7cca85cfca651983794615731bfb6fd16dfd6d
Genius_um <101275371+Geniusum@users.noreply.github.com>
2023-05-03 17:30:17 +0100

    Update gui.lua

--- gui.lua
+++ gui.lua
@@ -1,1 +1,16 @@
+-- MazeInstance OS Gui
 
+-- Make GUI
+
+-- Init wallpapers
+
+wallpaper_1 = paintutils.loadImage("./assets/wallpaper_1.nfp")
+
+function load_wallpaper_1()
+    term.setCursorPos(1, 1)
+    paintutils.drawImage(_dt, 1, 1)
+end
+
+while true do
+    i=1
+end
