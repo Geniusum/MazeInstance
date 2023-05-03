4a112f81d25ebe1052f3b5c2e27ba137bdbeaeb4
Genius_um <101275371+Geniusum@users.noreply.github.com>
2023-05-03 17:08:25 +0100

    Create lib-const-var.lua

---
+++ lib-const-var.lua
@@ -0,0 +1,12 @@
+-- MazeInstance OS lib/const_var
+
+-- OS Info :
+
+version = "1.0.0"
+author = "Genius_um"
+name = "MazeInstance"
+fast_name = "MI"
+
+-- Utils Info :
+
+wallpaper_prefix = "wallpaper_"
