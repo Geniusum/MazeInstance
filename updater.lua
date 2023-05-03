872484da8f26a06e7d8e4253662bb6b3efaee28e
Genius_um <101275371+Geniusum@users.noreply.github.com>
2023-05-03 17:02:48 +0100

    Update updater.lua

--- updater.lua
+++ updater.lua
@@ -1,1 +1,43 @@
--- test
+-- MazeInstance OS Updater
+
+
+print("MazeInstance OS Updater")
+
+sleep(1)
+
+-- Verification of rget existants
+
+function file_exists(file)
+    local f = io.open(file, "r")
+    if f then
+        f:close()
+        return true
+    end
+    return false
+end
+
+rget_existants = file_exists("rget.lua")
+
+if rget_existants == false then
+    print("[INFO] Program missing : rget is not installed on your computer.")
+    sleep(1)
+    print("[INFO] Installation of rget.")
+    print("")
+    shell.run("wget get https://raw.githubusercontent.com/Geniusum/CraftOS_Programs/main/rget.lua rget.lua")
+    print("")
+    print("[INFO] rget has installed.")
+end
+
+-- Now rget is ready, go update the OS !
+
+print("[INFO] Update of OS...")
+
+shell.run("cd ../../")
+shell.run("delete MazeInstance")
+shell.run("mkdir MazeInstance")    
+shell.run("cd MazeInstance")
+shell.run("mkdir system lib update files assets")
+shell.run("cd update")
+shell.run("copy ../../rget.lua .")
+shell.run("rget Geniusum MazeInstance OS updater.lua updater.lua")
+shell.run("cd ..")
